bb53ba3add2b063061fc2f6cb8352303140cb5c3
mdolitsoi <mdolitsoiv@gmail.com>
2024-05-26 13:41:01 +0300

    laba6

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/db_IO-24_team2/',
+  base: '/db_IO-24_team2_Dolitsoi/',
   theme: 'cool',
   // dest: 'dist',
   head: [
@@ -74,7 +74,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/lesflq/db_IO-24_team2',
+    repo: 'https://github.com/mdolitsoi/db_IO-24_team2_Dolitsoi',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
